8ac6e2d056f60f19e3c7249b2cd01f371dbc8659
Angel Almonte <56664605+angelrepublic24@users.noreply.github.com>
2025-04-11 21:22:53 -0400

    Update page.tsx

--- src/app/(InfoLayout)/project/[id]/page.tsx
+++ src/app/(InfoLayout)/project/[id]/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import React from "react";
 import Link from "next/link";
 
-export default async function ProjectPage({ params }: Awaited<{ params: { id: string } }>) {
+export default async function ProjectPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   const project = await getProjectById(id);
 
